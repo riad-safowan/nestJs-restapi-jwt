f514162f4f3876f4b96dcf82d13a3bf69bf2bb0a
Riad Safowan <riad.safowan@northsouth.edu>
2022-03-08 16:56:12 +0600

    entities model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, Post } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { User } from './user/user.entity';
@@ -6,6 +6,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
 import { LikeModule } from './like/auth.module';
+import { Post } from './post/post.entity';
+import { Comment } from './comment/comment.entity';
+import { Like } from './like/like.entity';
 
 @Module({
   imports: [
@@ -22,7 +25,7 @@ import { LikeModule } from './like/auth.module';
       username: 'root',
       password: '123456',
       database: 'nest',
-      entities: [User],
+      entities: [User, Post, Comment, Like],
       synchronize: true,
     }),
   ],
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,13 +1,43 @@
 import { NodeWorker } from 'inspector';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: string;
+  @Column()
+  first_name: string;
+  @Column()
+  last_name: string;
+  @Column()
+  image_url?: string;
+  @Column()
+  password: string;
+  @Column()
+  email: string;
+  @Column({ length: 500 })
+  access_token: string;
+  @Column({ length: 300 })
+  refresh_token: string;
+  @Column()
+  user_type: string;
 
-  firstName: string
-  lastName: string
-  ImageUrl?: string
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  created_at: Date;
 
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  updated_at: Date;
 }
